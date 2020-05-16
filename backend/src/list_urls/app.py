a50ec8d3cd1cc62a70704cd8e69164d713393ca0
Pubudu Jayawardana <pubudusj@gmail.com>
2020-05-17 00:38:45 +0200

    list urls functionality added

--- backend/src/list_urls/app.py
+++ backend/src/list_urls/app.py
@@ -1,9 +1,35 @@
 import json
+import boto3
+import os
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+client = boto3.resource('dynamodb')
+table = client.Table(os.getenv('DYNAMODB_TABLE'))
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
 
 def index(event, context):
+    response = table.scan(
+        FilterExpression=Key('type').eq('short-url'),
+        )
+
+    data = []
+    for item in response['Items']:        data.append({
+            'url': item.get('url'),
+            'short_code': item.get('pk')
+        })
+
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "Fetch All URLS",
-        }),
+        "body": json.dumps(data),
     }
